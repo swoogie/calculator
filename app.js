89d540de18b7e14436fe6a8004e6468aa0c76e79
Ignas Biekša <ignasbieksa@gmail.com>
2021-11-01 15:15:28 +0200

    updated

--- app.js
+++ app.js
@@ -1,44 +1,163 @@
-function sin(){
-	document.calcul.result.value=Math.sin(document.calcul.result.value);
-	}
 
-	function cos(){
-	document.calcul.result.value=Math.cos(document.calcul.result.value);
-	}
 
-	function tan(){
-	document.calcul.result.value=Math.tan(document.calcul.result.value);
-	}
+class Calculator{
+    constructor(lastOpText, curOpText) {
+        this.lastOpText = lastOpText;
+        this.curOpText = curOpText;
+        this.clear();
+    }
 
-	function BACKSPC(){
-	var a = document.calcul.result.value;
-	document.calcul.result.value = a.substr(0, a.length-1);
-	}
+    clear(){
+        this.curOp = '';
+        this.lastOp = '';
+        this.fun = null;
+    }
 
-	function square(){
-	document.calcul.result.value = Math.pow(document.calcul.result.value, 2);
-	}
+    delete(){
+        this.curOp = this.lastOp.toString().slice(0, -1)
+    }
 
-	function cubed(){
-	document.calcul.result.value = Math.pow(document.calcul.result.value, 3);
-	}
+    appendNumber(num){
+        if (this.curOp.includes('.') && num === '.'){
+            return;
+        }
+        this.curOp = this.curOp.toString() + num.toString;
+    }
 
-	function sqrt2(){
-	document.calcul.result.value = Math.pow(document.calcul.result.value, 1/2);
-	}
+    selectFun(fun){
+        if(this.curOp === '') {
+            this.calculate()
+        }
+        this.fun = fun;
+        this.lastOp = this.curOp;
+        this.curOp = '';
+    }
 
-	function sqrt3(){
-	document.calcul.result.value = Math.pow(document.calcul.result.value, 1/3);
-	}
+    calculate(){
+        let equal;
+        const cur = parseFloat(this.curOp);
+        const last = parseFloat(this.lastOp);
+        if (isNaN(last) || isNaN(cur)){
+            return;
+        }
+        switch(this.fun){
+            case '+':
+                equal = last + cur;
+                break;
+            case '-':
+                equal = last - cur;
+                break;
+            case '*':
+                equal = last * cur;
+                break;
+            case '/':
+                equal = last / cur;
+                break;
+            case '^':
+                equal = Math.pow(last, cur);
+                break;
+            case 'x√':
+                equal = Math.pow(last, 1/cur);
+                break;
+            default:
+                return;
+        }
+        this.curOp = equal;
+        this.fun = null;
+        this.lastOp = '';
+    }
 
-	function number(value){
-	document.calcul.result.value += value;
-	}
+    selectFunSingle(fun){
+        this.fun = fun;
+        if(this.curOp === ''){
+            return;
+        }
+        else{
+            this.funSingle();
+        }
+    }
 
-	function remv(){
-	document.calcul.result.value=" ";
-	}
+    funSingle(){
+        let equal;
+        const cur = parseFloat(this.curOp)
+        if (isNaN(cur)){
+            return;
+        }
+        switch(this.fun){
+            case 'sin':
+                equal = Math.sin(cur);
+                break;
+            case 'cos':
+                equal = Math.cos(cur);
+                break;
+            case 'tan':
+                equal = Math.tan(cur);
+                break;
+            case '√':
+                equal = Math.sqrt(cur);
+                break;
+            default:
+                return;
+        }
+        this.curOp = equal;
+        this.fun = null;
+    }
 
-	function equal(){
-	document.calcul.result.value=eval(document.calcul.result.value);
-	}
+    update(){
+        this.curOpText.innerText = this.curOp;
+        if (this.fun != null){
+            this.lastOpText.innerText = this.lastOp + this.fun;
+        }
+        else{
+            lastOpText = '';
+        }
+    }
+}
+
+const numButton = document.querySelectorAll(".numBtn");
+const funButton = document.querySelectorAll(".funBtn");
+const funSingle = document.querySelectorAll(".funSingle");
+const eqlButton = document.querySelector(".equal");
+const clrButton = document.querySelector(".clear");
+const delButton = document.querySelector(".delete");
+const lastOpText = doument.querySelector(".lastOp");
+const curOpText = document.querySelector(".curOp");
+
+const calculator = new Calculator(lastOpText, curOpText);
+
+numButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.update();
+    })
+})
+
+funButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.selectFun(button.innerText);
+        calculator.update();
+    })
+})
+
+funSingle.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.selectFunSingle(button.innerText);
+        calculator.update();
+    })
+})
+
+eqlButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+clrButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.update();
+})
+
+delButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.update();
+})
+
